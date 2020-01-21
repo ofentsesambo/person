7dd84c6d4bac06a7dc9b46d53b8e13fdae1374c3
Ofentse <ofentsesambo@gmail.com>
2020-01-21 11:38:07 +0200

    changes made

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,28 +1,28 @@
 let person = require("../src/person");
 
-let myPerson = new person('Ryan', 30, 'male',['being a hardarse','agile','ssd hard drives']);
+let myPerson = new person("Ryan", 30, "male", [
+  "being a hardarse",
+  "agile",
+  "ssd hard drives"
+]);
 
-//console.log(greeting);
+describe("hello funtion", function() {
+  let greeting = myPerson.hello();
+  it("should return Ryan", function() {
+    expect(myPerson.name).toBe("Ryan");
+  });
 
-describe('hello funtion', function(){
+  it("should return the age", function() {
+    expect(myPerson.age).toBe(30);
+  });
 
-    it('outputs a message', function(){
-        let greeting = myPerson.hello();
-        expect(myPerson.name).toBe('Ryan');
-    });
+  it("should return the gender", function() {
+    expect(myPerson.gender).toBe("male");
+  });
 
-    it('outputs a message', function(){
-        let greeting = myPerson.hello();
-        expect(myPerson.age).toBe(30);
-    });
-
-    it('outputs a message', function(){
-        let greeting = myPerson.hello();
-        expect(myPerson.gender).toBe('male');
-    });
-
-    it('outputs a message', function(){
-        let greeting = myPerson.hello();
-        expect(greeting).toBe('Hello my name is Ryan and i am 30 years old, my interest are being a hardarse, agile and ssd hard drives');
-    });
+  it("should return hello messege", function() {
+    expect(greeting).toBe(
+      "Hello my name is Ryan and i am 30 years old, my interest are being a hardarse, agile and ssd hard drives"
+    );
+  });
 });
